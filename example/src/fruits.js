2de7254eac2327a4c5f26a9e81309ccde59e82dc
Udasi Tharani <udasitharani@gmail.com>
2025-03-19 23:28:56 +0530

    wip

--- example/src/fruits.js
+++ example/src/fruits.js
@@ -1,8 +1,1 @@
-const apple = require("./apple");
-const banana = require("./banana");
-const guava = require("./guava");
-const papaya = require("./papaya");
-const strawberry = require("./strawberry");
-const watermelon = require("./watermelon");
-
-module.exports = [apple, banana, guava, papaya, strawberry, watermelon];
+module.exports = ["apple"];
--- index.js
+++ index.js
@@ -1,18 +1,123 @@
+// Things to think about:
+// 1. node module resolution algorithm
+// 2. .cjs & esm integration - if package is esm, code is cjs or vice versa, how will that work?
+// 3. Same dependency for many files
+// 4. node_modules
+// 5. minification
+//
+// 1. Start executing Code
+// 2. Encounter a dependency
+// 3. Bring code from there and cram into the bundle
+
+// function getCode(inputFile) {
+//   const code = read(inputFile);
+
+//   const dependencies = findDependencies(code);
+
+//   dependencies.forEach((dependency) => {
+//     const dependencyCode = getCode(dependency);
+//     code.replaceRequireWithCode(dependency, dependencyCode);
+//   });
+
+//   return code;
+// }
+
 const fs = require("fs");
+const path = require("path");
 
-async function readCode(inputFile) {
-  const code = await fs.promises.readFile(inputFile, "utf-8");
+/**
+ * @param {string} code
+ */
+async function generateDependencies(code, parentDir) {
+  const dependencies = [];
 
-  return code;
+  const requireStatements = code
+    .split("\n")
+    .filter((statement) => statement.includes("require("));
+
+  for (let statement of requireStatements) {
+    const requireExpression = statement.match(/require\(.+\)/)?.[0];
+    if (requireExpression) {
+      const start = requireExpression.indexOf("(") + 2;
+      const end = requireExpression.indexOf(")") - 1;
+
+      let inputFile = requireExpression.slice(start, end);
+      if (
+        (inputFile.includes("./") || inputFile.includes(".\\")) &&
+        !inputFile.endsWith(".js")
+      ) {
+        inputFile += ".js";
+      }
+      inputFile = path.resolve(parentDir, inputFile);
+
+      const code = await readCode(inputFile);
+
+      dependencies.push({
+        name: inputFile,
+        code: code,
+      });
+    }
+  }
+
+  return dependencies;
 }
 
-async function bundle(inputFile, output) {
-  const code = await readCode(inputFile);
+async function readCode(inputFile, bundle = "") {
+  // const moduleName = getModuleName(inputFile);
+  const moduleName = inputFile;
 
-  console.log(code);
+  const code = await fs.promises.readFile(inputFile, "utf-8");
+
+  const dependencies = await generateDependencies(
+    code,
+    path.dirname(inputFile)
+  );
+
+  dependencies.forEach((dependency) => {
+    bundle += `
+    modules["${dependency.name}"] = function () {
+      ${dependency.code}
+    }
+    `;
+  });
+
+  bundle += `
+  modules["${moduleName}"] = function () {
+    ${code}
+  }
+  `;
+
+  return bundle;
+}
+
+async function bundle(entryPoint, output) {
+  const code = await readCode(entryPoint);
+
+  const bundle = `
+      const modules = {};
+
+      function require(moduleName) {
+        const module = {exports: {}}
+
+
+        modules[moduleName](module); // someModuleExportsHereWhileRunningTheCodeInTheModule;
+        
+        return module.exports;
+      }
+
+      ${code}
+
+      modules["${entryPoint}"]()
+    `;
+
+  console.log(bundle);
+
+  // writeToFile(output, code);
 }
 
 const inputFile = process.argv[2];
 const output = process.argv[3];
 
-bundle(inputFile, output);
+const entryPoint = path.resolve(inputFile);
+
+bundle(entryPoint, output);
